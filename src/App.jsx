c16f85995ba69416add35e8986dc65d4cb7ee665
Kevin Poppe <kevin.poppe93@gmail.com>
2022-03-26 08:54:45 +0100

    resume, aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
+import Resume from "./components/Resume";
+
+import ProjectOverview from "./components/projects/ProjectOverview";
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects">
+          <Route index element={<ProjectOverview />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/components/Resume.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="resume-div">
+      <h1>Kevin Poppe</h1>
+      <p>Wohnahft in 97903 Collenberg</p>
+      <p>
+        Email: <a href="mailto:kevin@kevinpoppe.com">Kevin@KevinPoppe.com</a>
+      </p>
+      <p>
+        Mobil: <a href="tel:+4916091118502">+49 160/911 18 502</a>
+      </p>
+      <p>
+        GitHub: <a href="https://github.com/Teleturbis">Teleturbis</a>
+      </p>
+      <p>
+        LinkedIn:{" "}
+        <a href="https://www.linkedin.com/in/poppekevin1993">Kevin Poppe</a>
+      </p>
+      <br />
+      <h2>Sprachen</h2>
+      <p>Deutsch: Muttersprache</p>
+      <p>Englisch: B2</p>
+      <br />
+      <h2>Skills</h2>
+      <div className="resume-skills">
+        <h3>JavaScript</h3>
+        <h3>ReactJS</h3>
+        <h3>Git</h3>
+        <h3>GitHub</h3>
+        <h3>Contenful</h3>
+        <h3>CSS</h3>
+        <h3>HTML</h3>
+        <p>Bootstrap</p>
+        <p>Java</p>
+        <p>Arduino</p>
+        <p>Photoshop</p>
+        <p>Figma</p>
+        <p>Miro</p>
+        <p>Trello</p>
+        <p>SQL</p>
+        <p>PostgreSQL</p>
+        <p>NodeJS</p>
+        <p>ExpressJS</p>
+        <p>SCRUM</p>
+        <p>Pflegedokumentation</p>
+        <p>Deeskalation</p>
+        <p>Wundmanagment</p>
+      </div>
+      <br />
+      <h2 className="resume-end">
+        Für einen detailierten Lebenslauf kontaktiere mich gerne!
+      </h2>
+    </div>
+  );
+}
